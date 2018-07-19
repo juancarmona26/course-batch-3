53c0f3b6c02a840ae8c9937ab4e4abedb91d465f
Christian Garcia <chrigarc@chrigarc-laptop-dell>
2018-07-18 21:42:12 -0500

    ejercicio de estilos que dejo anuar

--- chrigarc/ejercicio_anuar/app.js
+++ chrigarc/ejercicio_anuar/app.js
@@ -1,15 +1,15 @@
-(function () {
+(() => {
     'use strict';
+
+    const showSidenav = () => {
+        sidenav.classList.toggle('show');
+    };
 
     const sidenavButtons = document.querySelectorAll('.sidenav-button');
     const sidenav = document.querySelector('.sidenav');
 
     for(const button of sidenavButtons) {
         button.addEventListener('click', showSidenav);
-    }
-
-    function showSidenav() {
-        sidenav.classList.toggle('show');
     }
 
 })();
