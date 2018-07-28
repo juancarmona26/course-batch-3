112e46f7d6cbc70675a223d3b7d3ca4b4a4e7323
juancarmona26 <juan.carmona.mtz@gmail.com>
2018-07-28 13:53:14 -0500

    se genera filtro

--- juancarmona26/Examen3/modulizer_out/src/views/home-view.js
+++ juancarmona26/Examen3/modulizer_out/src/views/home-view.js
@@ -1,4 +1,6 @@
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
+import '@polymer/paper-button/paper-button.js';
+import '@polymer/paper-input/paper-input.js';
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 /**
  * @customElement
@@ -16,13 +18,61 @@ class HomeView extends PolymerElement {
           background-color: var(--paper-green-500);
           color: white;
       }
+      * {
+      margin: 0;
+      padding: 0;
+    }
+    
+    :root {
+      --secondary-color: #555;
+      --box-height: 100px;
+      --font-size: 20px;
+    }
+  
+    .box-model {
+      border: 3px dashed var(--secondary-color);
+      margin: 20px;
+      padding: 20px;
+    }
+  
+    ul {
+      list-style-type: none;
+    }
+  
+    .section__title {
+      text-align: center;
+      margin: 10px 0 25px 0;
+    }
+  
+  .grid {
+      border: 1px solid blue;
+      padding: 20px;
+      display: grid;
+    
+      /* grid-template-columns: 1fr  2fr; */
+      /* grid-template-rows: 200px 59px 200px ; */
+    
+      grid-template-columns: repeat(4, 1fr) ;
+      /* grid-template-columns: 1fr .6fr 1fr 1fr; */
+      /* grid-template-columns: 1fr 1fr 1fr 1fr; */
+      grid-gap: 50px;
+    }
+    
+    .grid-item {
+      border: 2px solid var(--secondary-color);
+      height: 100%;
+      width: 100%;
+      text-align: center;
+    }
     </style>    
 
     <section class="box-model">
     <h2 class="section__title">Showing Characters</h2>
+    <paper-input id="filterInput" value="{{filterVal::input}}" label="Filtro"></paper-input>
+       
   <ul class="grid">
     
-    <template is="dom-repeat" items="[[response]]" as="character" index-as="indexA" >
+    <template is="dom-repeat" items="{{response}}" as="character" index-as="indexA" filter="{{_filter(filterVal)}}" >
     <article class="" >
       <div class="grid-item">
         <div><img src="{{character.avatar}}" alt="Character">  </div>
@@ -56,8 +106,16 @@ class HomeView extends PolymerElement {
           value:[]                
         }
     
-    }
+    }  
   }
+  
+  _filter(val)  {  
+    return (character) => {
+      if (!val) return true;
+      if (!character) return false;
+      return (character.first_name && ~character.first_name.indexOf(val));
+    };
+  };
 }
 
 window.customElements.define(HomeView.is, HomeView);
