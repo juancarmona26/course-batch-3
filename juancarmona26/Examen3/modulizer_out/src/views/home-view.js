19c2051b9f8e0d7ae973821bb73431e787f9884f
juancarmona26 <juan.carmona.mtz@gmail.com>
2018-07-28 15:36:58 -0500

    función de ordenamiento por parámetros

--- juancarmona26/Examen3/modulizer_out/src/views/home-view.js
+++ juancarmona26/Examen3/modulizer_out/src/views/home-view.js
@@ -1,6 +1,7 @@
 import { PolymerElement } from '@polymer/polymer/polymer-element.js';
 import '@polymer/paper-button/paper-button.js';
 import '@polymer/paper-input/paper-input.js';
+import '@polymer/iron-image/iron-image.js'
 import { html } from '@polymer/polymer/lib/utils/html-tag.js';
 /**
  * @customElement
@@ -64,18 +65,51 @@ class HomeView extends PolymerElement {
       width: 100%;
       text-align: center;
     }
+    .thumbnail {
+      height: 120px;
+      width: 120px;
+      margin:10px 0 0 0;
+      border-radius: 70%;
+    }
+    .select {
+      border: 1px solid #ccc;
+      width: 140px;
+      margin: 10px auto 30px;
+      overflow: hidden;
+      background: #fff url("http://www.csslab.cl/ejemplos/select/arrowdown.gif") no-repeat 90% center;
+    }
+    .select select {
+        padding: 5px 8px;
+        width: 130%;
+        border: none;
+        box-shadow: none;
+        background-color: transparent;
+        background-image: none;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        appearance: none;
+    }
     </style>    
 
     <section class="box-model">
     <h2 class="section__title">Showing Characters</h2>
     <paper-input id="filterInput" value="{{filterVal::input}}" label="Filtro"></paper-input>
-       
+    <p>Ordenar por: </p>
+    <div class="select">
+    
+      <select value="{{sortVal::change}}">  
+        <option value="id_user">User Id</option>
+        <option value="first_name">First Name</option>
+        <option value="last_name">Last Name</option>
+      </select>  
+    </div>
   <ul class="grid">
     
-    <template is="dom-repeat" items="{{response}}" as="character" index-as="indexA" filter="{{_filter(filterVal)}}" >
+    <template is="dom-repeat" items="{{response}}" as="character" index-as="indexA" filter="{{_filter(filterVal)}}" 
+    sort="{{_sortByName(sortVal)}}">
     <article class="" >
       <div class="grid-item">
-        <div><img src="{{character.avatar}}" alt="Character">  </div>
+        <div><iron-image src="{{character.avatar}}" class="thumbnail" sizing="cover" preload fade> </iron-image> </div>
             
             <li>
 
@@ -104,7 +138,12 @@ class HomeView extends PolymerElement {
         response: {
           type:Array,
           value:[]                
+        },
+        employees: {
+          type:Array,
+          value:['hola1', 'hola2']                
         }
+
     
     }  
   }
@@ -116,6 +155,26 @@ class HomeView extends PolymerElement {
       return (character.first_name && ~character.first_name.indexOf(val));
     };
   };
+
+  _sortByName(val){
+    switch(val) {
+      case 'first_name':
+        return (a, b) => {
+          if (a.first_name === b.first_name) return 0;
+          return a.first_name < b.first_name ? -1 : 1;
+        };
+      case 'id_user':
+        return (a, b) => {
+          if (a.id === b.id) return 0;
+          return a.id < b.id ? -1 : 1;
+        };
+      case 'last_name':      
+        return (a, b) => {
+          if (a.last_name === b.last_name) return 0;
+          return a.last_name < b.last_name ? -1 : 1;
+        };
+    }
+  }
 }
 
 window.customElements.define(HomeView.is, HomeView);
